4ac448c0836dc90812fd9abf8e71f7630bceae7f
Robert Svart <robnoir@Roberts-MacBook-Air.local>
2023-11-17 05:40:03 +0100

    commit all changes and finalise project

--- Skottår Kalkylator/År.cs
+++ Skottår Kalkylator/År.cs
@@ -4,6 +4,8 @@ namespace Skottår_Kalkylator
 {
     public class År
     {
+
+    
         public static bool ÄrSkottår(int år)
         {
             return (år % 4 == 0 && år % 100 !=0) || (år % 400 == 0);
